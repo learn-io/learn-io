be105741c835a84519aa02b7ee433a131379d354
xin0415 <34253658+xin0415@users.noreply.github.com>
2021-03-09 01:58:47 -0500

    start
    start the project

---
+++ src/Platform.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Platform =()=>{
+	return(
+		<div>
+			<img alt='platformImage' src='https://robohash.org/test?200x200'/>
+			<div>
+				<h3>Tuvan Throat Singing Facts</h3>
+			</div>
+		</div>
+	);
+}
+export default Platform;
