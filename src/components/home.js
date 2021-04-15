ebd4e69db9b586eda6b0537210674452b94f2762
xin0415 <34253658+xin0415@users.noreply.github.com>
2021-04-15 15:01:17 -0400

    update home

--- src/components/home.js
+++ src/components/home.js
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from 'react';
 import './ComponentStyle.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Dropdown} from 'react-bootstrap';
+import uploadIcon from './images/upload.png';
+import editIcon from './images/edit.png';
+import saveIcon from './images/save.png';
 
 const HomeController = (props) =>{
     const [platforms, setPlatforms] = useState([])
@@ -11,6 +14,11 @@ const HomeController = (props) =>{
     const [limit, setLimit] = useState(10);
     const [nextPlatforms, setNextPlatforms] = useState(false);
     const [selectPlatform, setSelectPlatform] = useState("");
+    const [header,setHeader]=useState("");
+    const [changeHeader,setChangeHeader]=useState(false);
+    const [description,setDescription]=useState("");
+    const [changeD,setChangeD]=useState(false);
+    const [save,setSave]=useState(0);
 
     useEffect(
         ()=>{
@@ -33,14 +41,63 @@ const HomeController = (props) =>{
             }).catch(function(err){
                 console.log(err);
             });
-        },[text, skip, limit]
+        },[text, skip, limit,save]
     );
+
+    const onSavePlatformInfo=(platform)=>{
+    	axios_instance({
+            method: 'post',
+            url: "platform/about",
+            data: {
+                _id: platform._id,
+                platformName:header,
+                image:"",
+                description:description
+            }
+        })
+        .then((res)=>{
+        	setSave(save+1);
+	    })
+	    .catch(err=>console.log(err));
+    }
+
+    const onUploadImage=()=>{
+    	console.log('upload');
+    }
+
+    const onEditHeader=(value)=>{
+    	if(changeHeader===false){
+    		setChangeHeader(true);
+    	}else{
+    		setChangeHeader(false);
+    		if(value!==""){
+    			setHeader(value);
+    		}
+    		
+    	}
+    }
+    const onEditDescription=(value)=>{
+    	if(changeD===false){
+    		setChangeD(true);
+    	}else{
+    		setChangeD(false);
+    		if(value!==""){
+    			setDescription(value);
+    		}
+    	}
+    }
 
     const onSelectPlatform=(platform)=>{
 		if(platform!==''){
 			setSelectPlatform(platform);
+			setHeader(platform.platformName);
+			setDescription(platform.description);
 		}else{
 			setSelectPlatform('');
+			setHeader('');
+			setDescription('');
+			setChangeHeader(false);
+			setChangeD(false);
 		}
 	}
     const onSearchPlatform=(value)=>{
@@ -52,48 +109,146 @@ const HomeController = (props) =>{
     	setSkip(0);
     }
     return (
-    <div className='appStyle'>
-        <SearchBox onSearchPlatform={onSearchPlatform} />
-        <Dropdown>
-			<Dropdown.Toggle style={{backgroundColor: '#cdecff',color:'#000'}} variant="success" id="dropdown-basic">
-		    	Platfroms per page: {limit}
-			</Dropdown.Toggle>
-			<Dropdown.Menu>
-				<Dropdown.Item onClick={()=>{onChangeLimit(3)}}>3</Dropdown.Item>
-		    	<Dropdown.Item onClick={()=>{onChangeLimit(10)}}>10</Dropdown.Item>
-		    	<Dropdown.Item onClick={()=>{onChangeLimit(15)}}>15</Dropdown.Item>
-		    	<Dropdown.Item onClick={()=>{onChangeLimit(20)}}>20</Dropdown.Item>
-			</Dropdown.Menu>
-		</Dropdown>
-        <PlatformList platforms={platforms} onSelectPlatform={onSelectPlatform}/>
-       	<PreviousButton limit={limit} skip={skip} setSkip={setSkip} />
-       	<ConfirmBox selectPlatform={selectPlatform} onSelectPlatform={onSelectPlatform}/>
-        <NextButton limit={limit} nextPlatforms={nextPlatforms} skip={skip} setSkip={setSkip}/>
-    </div>
+	    <div className='appStyle'>
+	        <SearchBox onSearchPlatform={onSearchPlatform} />
+	        <Dropdown>
+				<Dropdown.Toggle style={{backgroundColor: '#cdecff',color:'#000'}} variant="success" id="dropdown-basic">
+			    	Platfroms per page: {limit}
+				</Dropdown.Toggle>
+				<Dropdown.Menu>
+					<Dropdown.Item onClick={()=>{onChangeLimit(3)}}>3</Dropdown.Item>
+			    	<Dropdown.Item onClick={()=>{onChangeLimit(10)}}>10</Dropdown.Item>
+			    	<Dropdown.Item onClick={()=>{onChangeLimit(15)}}>15</Dropdown.Item>
+			    	<Dropdown.Item onClick={()=>{onChangeLimit(20)}}>20</Dropdown.Item>
+				</Dropdown.Menu>
+			</Dropdown>
+	        <PlatformList platforms={platforms} onSelectPlatform={onSelectPlatform}/>
+	       	<PreviousButton limit={limit} skip={skip} setSkip={setSkip} />
+	       	<ConfirmBox
+	       		username={props.username}
+	       		header={header}
+		        description={description}
+	       		selectPlatform={selectPlatform}
+	       		onSelectPlatform={onSelectPlatform}
+	       		onSavePlatformInfo={onSavePlatformInfo}
+	        	onUploadImage={onUploadImage}
+	        	onEditHeader={onEditHeader}
+	        	onEditDescription={onEditDescription}
+	        	changeHeader={changeHeader}
+	        	changeD={changeD}
+	       	/>
+	        <NextButton limit={limit} nextPlatforms={nextPlatforms} skip={skip} setSkip={setSkip}/>
+	    </div>
     )
 }
 
-const ConfirmBox=({selectPlatform,onSelectPlatform})=>{
+const ConfirmBox=({username,header,description,selectPlatform,onSelectPlatform,onSavePlatformInfo,onUploadImage,onEditHeader,onEditDescription,changeHeader,changeD})=>{
 	if(selectPlatform==='') return null
-	return (
-		<section id="overlay">
-			<div className='overlayStyle'>
-				<div className='selectConfirm'>
-					<button className='closeButton' onClick={()=>{onSelectPlatform('')}}>X</button>
-					<h2>{selectPlatform.platformName}</h2>
-					<div className='selectImage'>
-						<img alt='platformImage' src={`https://robohash.org/${selectPlatform.platformName}?200x200`}/>
-					</div>
-					<div>
-						<p className='paragraph'>{selectPlatform.description}</p>
-					</div>
-					<div className='clearfix'>
-						<button className='playButton'>Play</button>
+	// let closehdr;
+	// let titlehdr:
+	// if(username!==selectPlatform.owner){
+	// 	closehdr=<button className='closeButton' onClick={()=>{onSelectPlatform('')}}>X</button>;
+	// 	titlehdr=<h2>{header}</h2>
+	// }else{
+	// 	closehdr=<div style={{justifyContent:'space-between',display:'flex'}}>
+	// 		<button className='deleteButton' onClick={()=>{onSavePlatformInfo(selectPlatform)}}><img src={saveIcon} height='50px' width='50px' alt="save"/></button>
+	// 		<button className='closeButton' onClick={()=>{onSelectPlatform('')}}>X</button>
+	// 	</div>
+	// 	let hdr;
+	// 	let hdrButton;
+	// 	if(changeHeader){
+	// 		hdr=<input style={{width:'50%'}} type="text" id="header" name="header"/>
+	// 		hdrButton=<button className='deleteButton' onClick={()=>{onEditHeader(document.getElementById("header").value)}}><img src={editIcon} height='50px' width='50px' alt="edit"/></button>
+	// 	}
+	// 	else{
+	// 		hdr=<h2>{header}</h2>
+	// 		hdrButton=<button className='deleteButton' onClick={()=>{onEditHeader()}}><img src={editIcon} height='50px' width='50px' alt="edit"/></button>
+	// 	}
+	// 	let desc;
+	// 	let descButton;
+	// 	if(changeD){
+	// 		desc=<input style={{width:'40%'}} type="text" id="desc" name="desc"/>
+	// 		descButton=<button className='deleteButton' onClick={()=>{onEditDescription(document.getElementById("desc").value)}}><img src={editIcon} height='50px' width='50px' alt="edit"/></button>
+	// 	}
+	// 	else{
+	// 		desc=<p className='paragraph'>{description}</p>
+	// 		descButton=<button className='deleteButton' onClick={()=>{onEditDescription()}}><img src={editIcon} height='50px' width='50px' alt="edit"/></button>
+	// 	}
+	// 	titlehdr=<div style={{justifyContent:'center',display:'flex'}}>
+	//  				{hdr}
+ // 					{hdrButton}
+	// 			</div>
+	// }
+	if(username!==selectPlatform.owner){
+		return (
+			<section id="overlay">
+				<div className='overlayStyle'>
+					<div className='selectConfirm'>
+						<button className='closeButton' onClick={()=>{onSelectPlatform('')}}>X</button>
+						<h2>{selectPlatform.platformName}</h2>
+						<div className='selectImage'>
+							<img alt='platformImage' src={`https://robohash.org/${selectPlatform.platformName}?200x200`}/>
+						</div>
+						<div>
+							<p className='paragraph'>{selectPlatform.description}</p>
+						</div>
+						<div className='clearfix'>
+							<button className='playButton'>Play</button>
+						</div>
 					</div>
 				</div>
-			</div>
-		</section>
-	);
+			</section>
+		);
+	}else{
+		let hdr;
+		let hdrButton;
+		if(changeHeader){
+			hdr=<input style={{width:'50%'}} type="text" id="header" name="header"/>
+			hdrButton=<button className='deleteButton' onClick={()=>{onEditHeader(document.getElementById("header").value)}}><img src={editIcon} height='50px' width='50px' alt="edit"/></button>
+		}
+		else{
+			hdr=<h2>{header}</h2>
+			hdrButton=<button className='deleteButton' onClick={()=>{onEditHeader()}}><img src={editIcon} height='50px' width='50px' alt="edit"/></button>
+		}
+		let desc;
+		let descButton;
+		if(changeD){
+			desc=<input style={{width:'40%'}} type="text" id="desc" name="desc"/>
+			descButton=<button className='deleteButton' onClick={()=>{onEditDescription(document.getElementById("desc").value)}}><img src={editIcon} height='50px' width='50px' alt="edit"/></button>
+		}
+		else{
+			desc=<p className='paragraph'>{description}</p>
+			descButton=<button className='deleteButton' onClick={()=>{onEditDescription()}}><img src={editIcon} height='50px' width='50px' alt="edit"/></button>
+		}
+		return (
+			<section id="overlay">
+				<div className='overlayStyle'>
+					<div className='selectConfirm'>
+						<div style={{justifyContent:'space-between',display:'flex'}}>
+							<button className='deleteButton' onClick={()=>{onSavePlatformInfo(selectPlatform)}}><img src={saveIcon} height='50px' width='50px' alt="save"/></button>
+							<button className='closeButton' onClick={()=>{onSelectPlatform('')}}>X</button>
+						</div>
+						<div style={{justifyContent:'center',display:'flex'}}>
+							{hdr}
+							{hdrButton}
+						</div>
+						<div style={{justifyContent:'space-between',display:'flex'}}>
+							<button className='deleteButton' onClick={()=>{onUploadImage()}}><img src={uploadIcon} height='50px' width='50px' alt="upload"/></button>
+							{descButton}
+						</div>
+						<div style={{justifyContent:'center',display:'flex'}}>
+							<img alt='platformImage' src={`https://robohash.org/${selectPlatform.platformName}?200x200`}/>
+							{desc}
+						</div>
+						<div className='clearfix'>
+							<button className='playButton'>Play</button>
+						</div>
+					</div>
+				</div>
+			</section>
+		);
+	}
+	
 }
 
 const Platform =({name,platform,onSelectPlatform})=>{
