027e6f5e504f20de4c8c38941f5bdf05ffcab4ae
xin0415 <34253658+xin0415@users.noreply.github.com>
2021-04-15 15:22:16 -0400

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -144,62 +144,21 @@ const HomeController = (props) =>{
 
 const ConfirmBox=({username,header,description,selectPlatform,onSelectPlatform,onSavePlatformInfo,onUploadImage,onEditHeader,onEditDescription,changeHeader,changeD})=>{
 	if(selectPlatform==='') return null
-	// let closehdr;
-	// let titlehdr:
-	// if(username!==selectPlatform.owner){
-	// 	closehdr=<button className='closeButton' onClick={()=>{onSelectPlatform('')}}>X</button>;
-	// 	titlehdr=<h2>{header}</h2>
-	// }else{
-	// 	closehdr=<div style={{justifyContent:'space-between',display:'flex'}}>
-	// 		<button className='deleteButton' onClick={()=>{onSavePlatformInfo(selectPlatform)}}><img src={saveIcon} height='50px' width='50px' alt="save"/></button>
-	// 		<button className='closeButton' onClick={()=>{onSelectPlatform('')}}>X</button>
-	// 	</div>
-	// 	let hdr;
-	// 	let hdrButton;
-	// 	if(changeHeader){
-	// 		hdr=<input style={{width:'50%'}} type="text" id="header" name="header"/>
-	// 		hdrButton=<button className='deleteButton' onClick={()=>{onEditHeader(document.getElementById("header").value)}}><img src={editIcon} height='50px' width='50px' alt="edit"/></button>
-	// 	}
-	// 	else{
-	// 		hdr=<h2>{header}</h2>
-	// 		hdrButton=<button className='deleteButton' onClick={()=>{onEditHeader()}}><img src={editIcon} height='50px' width='50px' alt="edit"/></button>
-	// 	}
-	// 	let desc;
-	// 	let descButton;
-	// 	if(changeD){
-	// 		desc=<input style={{width:'40%'}} type="text" id="desc" name="desc"/>
-	// 		descButton=<button className='deleteButton' onClick={()=>{onEditDescription(document.getElementById("desc").value)}}><img src={editIcon} height='50px' width='50px' alt="edit"/></button>
-	// 	}
-	// 	else{
-	// 		desc=<p className='paragraph'>{description}</p>
-	// 		descButton=<button className='deleteButton' onClick={()=>{onEditDescription()}}><img src={editIcon} height='50px' width='50px' alt="edit"/></button>
-	// 	}
-	// 	titlehdr=<div style={{justifyContent:'center',display:'flex'}}>
-	//  				{hdr}
- // 					{hdrButton}
-	// 			</div>
-	// }
+	let closehdr;
+	let titlehdr;
+	let centerpart;
 	if(username!==selectPlatform.owner){
-		return (
-			<section id="overlay">
-				<div className='overlayStyle'>
-					<div className='selectConfirm'>
-						<button className='closeButton' onClick={()=>{onSelectPlatform('')}}>X</button>
-						<h2>{selectPlatform.platformName}</h2>
-						<div className='selectImage'>
-							<img alt='platformImage' src={`https://robohash.org/${selectPlatform.platformName}?200x200`}/>
-						</div>
-						<div>
-							<p className='paragraph'>{selectPlatform.description}</p>
-						</div>
-						<div className='clearfix'>
-							<button className='playButton'>Play</button>
-						</div>
-					</div>
-				</div>
-			</section>
-		);
+		closehdr=<button className='closeButton' onClick={()=>{onSelectPlatform('')}}>X</button>
+		titlehdr=<h2>{header}</h2>
+		centerpart=<div style={{justifyContent:'center',display:'flex'}}>
+ 						<img alt='platformImage' src={`https://robohash.org/${selectPlatform.platformName}?200x200`}/>
+ 						<p className='paragraph'>{description}</p>
+	 				</div>
 	}else{
+		closehdr=<div style={{justifyContent:'space-between',display:'flex'}}>
+			<button className='deleteButton' onClick={()=>{onSavePlatformInfo(selectPlatform)}}><img src={saveIcon} height='50px' width='50px' alt="save"/></button>
+			<button className='closeButton' onClick={()=>{onSelectPlatform('')}}>X</button>
+		</div>
 		let hdr;
 		let hdrButton;
 		if(changeHeader){
@@ -220,19 +179,11 @@ const ConfirmBox=({username,header,description,selectPlatform,onSelectPlatform,o
 			desc=<p className='paragraph'>{description}</p>
 			descButton=<button className='deleteButton' onClick={()=>{onEditDescription()}}><img src={editIcon} height='50px' width='50px' alt="edit"/></button>
 		}
-		return (
-			<section id="overlay">
-				<div className='overlayStyle'>
-					<div className='selectConfirm'>
-						<div style={{justifyContent:'space-between',display:'flex'}}>
-							<button className='deleteButton' onClick={()=>{onSavePlatformInfo(selectPlatform)}}><img src={saveIcon} height='50px' width='50px' alt="save"/></button>
-							<button className='closeButton' onClick={()=>{onSelectPlatform('')}}>X</button>
-						</div>
-						<div style={{justifyContent:'center',display:'flex'}}>
-							{hdr}
-							{hdrButton}
-						</div>
-						<div style={{justifyContent:'space-between',display:'flex'}}>
+		titlehdr=<div style={{justifyContent:'center',display:'flex'}}>
+	 				{hdr}
+ 					{hdrButton}
+				</div>
+		centerpart=<>	<div style={{justifyContent:'space-between',display:'flex'}}>
 							<button className='deleteButton' onClick={()=>{onUploadImage()}}><img src={uploadIcon} height='50px' width='50px' alt="upload"/></button>
 							{descButton}
 						</div>
@@ -240,15 +191,22 @@ const ConfirmBox=({username,header,description,selectPlatform,onSelectPlatform,o
 							<img alt='platformImage' src={`https://robohash.org/${selectPlatform.platformName}?200x200`}/>
 							{desc}
 						</div>
-						<div className='clearfix'>
-							<button className='playButton'>Play</button>
-						</div>
+					</>
+	}
+	return (
+		<section id="overlay">
+			<div className='overlayStyle'>
+				<div className='selectConfirm'>
+					{closehdr}
+					{titlehdr}
+					{centerpart}
+					<div className='clearfix'>
+						<button className='playButton'>Play</button>
 					</div>
 				</div>
-			</section>
-		);
-	}
-	
+			</div>
+		</section>
+	);
 }
 
 const Platform =({name,platform,onSelectPlatform})=>{
