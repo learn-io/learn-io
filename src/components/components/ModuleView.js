4e25fe1b05312a96a476334f63c02193897d3549
xin0415 <34253658+xin0415@users.noreply.github.com>
2021-04-22 03:47:14 -0400

    fix get params

--- src/components/components/ModuleView.js
+++ src/components/components/ModuleView.js
@@ -4,100 +4,32 @@ import '../ComponentStyle.css';
 import ModuleList from './ModuleList.js'
 import axios_instance from '../axios_instance.js';
 
-const ModuleView=(props)=>{
+const ModuleView=()=>{
 	const [platform,setPlatform]=useState("");
-    let { id } = useParams();
+    let { platformId } = useParams();
 	useEffect(
         ()=>{
         	axios_instance({
 		        method: 'get',
-		        url: "platform/"+id
+		        url: "platform/"+platformId
 		    }).then(function(response){
-		    	setPlatform(response.data)
+		    	setPlatform(response.data);
 		    }).catch(function(err){
 		        console.log(err);
 		    });
-        },[id]
+        },[platformId]
     );
-
-	let modules=[{
-        "platformId": "607b74dc4165c90aa0dfdce5",
-        "moduleName": "Obscure Berries",
-        "moduleDescription": "This is a berry test!!",
-        "lockedby": [],
-        "unlocks": [],
-        "x": 0,
-        "y": 0,
-        "height": 130,
-        "width": 130
-    }, {
-        "platformId": "607b74dc4165c90aa0dfdce5",
-        "moduleName": "What are Common Berries?",
-        "moduleDescription": "Guess what is a berry test!!",
-        "lockedby": [0],
-        "unlocks": [2],
-        "x": 100,
-        "y": 50,
-        "height": 130,
-        "width": 130
-    }, {
-        "platformId": "607b74dc4165c90aa0dfdce5",
-        "moduleName": "What are Botany Berries?",
-        "moduleDescription": "berry berry!!",
-        "lockedby": [1],
-        "unlocks": [3],
-        "x": 120,
-        "y": 430,
-        "height": 130,
-        "width": 130
-    }, {
-        "platformId": "607b74dc4165c90aa0dfdce5",
-        "moduleName": "Bananas?",
-        "moduleDescription": "Does bananas belong to berry?!",
-        "lockedby": [2],
-        "unlocks": [4],
-        "x": 140,
-        "y": 410,
-        "height": 130,
-        "width": 130
-    }, {
-        "platformId": "607b74dc4165c90aa0dfdce5",
-        "moduleName": "Watermelon!",
-        "moduleDescription": "How about watermelon",
-        "lockedby": [3],
-        "unlocks": [5],
-        "x": 130,
-        "y": 510,
-        "height": 130,
-        "width": 130
-    }, {
-        "platformId": "607b74dc4165c90aa0dfdce5",
-        "moduleName": "Lemons?",
-        "moduleDescription": "Is it a berry?",
-        "lockedby": [4],
-        "unlocks": [6],
-        "x": 380,
-        "y": 808,
-        "height": 300,
-        "width": 300
-    }, {
-        "platformId": "607b74dc4165c90aa0dfdce5",
-        "moduleName": "Pumpkins!?",
-        "moduleDescription": "How about halloween pumpkins!!",
-        "lockedby": [5],
-        "unlocks": [],
-        "x": 77,
-        "y": 100,
-        "height": 130,
-        "width": 130
-    }];
-
-	return(
+   
+    if(platform===''){
+        return (<h2 style={{color:'white'}}>{platform.platformName}</h2>);
+    }else{
+        return(
             <>
-				<h2>{id}</h2>
-				<ModuleList platform={platform} modules={modules}/>
+				<h2 style={{color:'white'}}>{platform.platformName}</h2>
+				<ModuleList platform={platform} modules={platform.modules}/>
             </>
-	);
+	    );
+    }
 }
 
 export default ModuleView;
--- src/components/platform.js
+++ src/components/platform.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './ComponentStyle.css';
-
 // import axios_instance from './axios_instance';
 import ModuleView from './components/ModuleView.js'
 
@@ -8,15 +7,16 @@ const Switch = require("react-router-dom").Switch;
 const Route = require("react-router-dom").Route;
 
 const PlatformController=()=>{
+
     return (
         <Switch>
-            <Route path="/:platform/:module/:page">
+            <Route path="/platform/:platformId">
                 <ModuleView/>
             </Route>
             <Route path="/:platform/:module">
                 <ModuleView/>
             </Route>
-            <Route path="/:platform">
+            <Route path="/:platform/:module/:page">
                 <ModuleView/>
             </Route>
         </Switch>
