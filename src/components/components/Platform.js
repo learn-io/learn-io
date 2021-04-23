eb02aa5d7dcdff3cd9a3961f67fd67e3c38c84fb
xin0415 <34253658+xin0415@users.noreply.github.com>
2021-04-23 01:25:57 -0400

    show image for platform

--- src/components/components/Platform.js
+++ src/components/components/Platform.js
@@ -2,9 +2,16 @@ import React from 'react';
 import '../ComponentStyle.css';
 
 const Platform =({name,platform,setSelectPlatform})=>{
+	let showImg;
+	if(platform.image===''){
+		showImg=<img alt='platformImage' src={`https://robohash.org/${name}?200x200`}/>
+	}else{
+		showImg=<img alt='platformImage' src={platform.image} height={200} width={200}/>
+	}
+	
 	return(
 		<div className='platformStyle grow' onClick={()=>{setSelectPlatform(platform)}}>
-			<img alt='platformImage' src={`https://robohash.org/${name}?200x200`}/>
+			{showImg}
 			<div>
 				<h4>{name}</h4>
 			</div>
