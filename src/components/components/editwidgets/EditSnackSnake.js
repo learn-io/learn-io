7272f38b2823b39a234df75847ee38b96fe4a5c9
xin0415 <34253658+xin0415@users.noreply.github.com>
2021-05-18 13:21:05 -0400

    Update EditSnackSnake.js

--- src/components/components/editwidgets/EditSnackSnake.js
+++ src/components/components/editwidgets/EditSnackSnake.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import {Table} from 'react-bootstrap';
+import React, {useEffect, useState} from 'react';
+import {Table,Button} from 'react-bootstrap';
 import './editStyle.css';
+import axios_instance from '../../axios_instance.js';
 
 const EditSnackSnake = ({selectedWidget,curPage,add,setAdd,pages})=>{
-
+    const [update,setUpdate]= useState(0);
+    // const [textArray,setTextArray]= useState([]);
+    // const hiddenFileInput = React.useRef(null);
+    useEffect(
+        ()=>{
+        },[update]
+    );
+    
+    // const handleClick = (event,index) => {
+    //     console.log(event);
+    //     console.log(hiddenFileInput.current);
+	// 	hiddenFileInput.current.click();
+	// };
     const onChangeAnswer=(event,answer)=>{
         for(let i=0;i<curPage.widgets.length;i++){
             if(curPage.widgets[i]===selectedWidget){
@@ -29,21 +42,58 @@ const EditSnackSnake = ({selectedWidget,curPage,add,setAdd,pages})=>{
     //         }
     //     }
     // }
-    const onChangeSnack=(event,position,index)=>{
-        for(let i=0;i<curPage.widgets.length;i++){
-            if(curPage.widgets[i]===selectedWidget){
-                while(curPage.widgets[i].internals.options.length<=index){
-                    let ob={"rightImage":"","wrongImage":""}
-                    curPage.widgets[i].internals.options.push(ob);
+    const onChangeSnack=(event,position,index,type)=>{
+        if(type==="text"){
+            for(let i=0;i<curPage.widgets.length;i++){
+                if(curPage.widgets[i]===selectedWidget){
+                    if(position==="correct"){
+                        curPage.widgets[i].internals.options[index].rightText=event.target.value;
+                    }else{
+                        curPage.widgets[i].internals.options[index].wrongText=event.target.value;
+                    }
+                    // console.log(curPage.widgets[i].internals.options);
+                    break;
                 }
-                if(position==="correct"){
-                    curPage.widgets[i].internals.options[index].rightImage=event.target.value;
-                }else{
-                    curPage.widgets[i].internals.options[index].wrongImage=event.target.value;
-                }
-                break;
+            }
+        }else{
+            if (event.target.files && event.target.files[0]) {
+                let imageFile = event.target.files[0];
+                let imageExtension = event.target.files[0].type;
+                    
+                let reader = new FileReader();
+                reader.onload = (e) => {
+                    let form = new FormData();
+                    form.append('file', e.target.result);
+                    form.append('extension', imageExtension);
+                    axios_instance({
+                        method: 'post',
+                        url: "media/",
+                        data: form,
+                        headers: {
+                            'Content-Type': `multipart/form-data; boundary=${form._boundary}`,
+                        },
+                    }).then((res)=>{
+                        // setImageHash(res.data.hash);
+                        for(let i=0;i<curPage.widgets.length;i++){
+                            if(curPage.widgets[i]===selectedWidget){
+                                if(position==="correct"){
+                                    curPage.widgets[i].internals.options[index].rightImage=res.data.hash;
+                                }else{
+                                    curPage.widgets[i].internals.options[index].wrongImage=res.data.hash;
+                                }
+                                // console.log(curPage.widgets[i].internals.options);
+                                break;
+                            }
+                        }
+                    }).catch((e)=>{
+                        console.log(e);
+                    })
+                };
+                reader.readAsDataURL(imageFile);
+                setUpdate(update+1);
             }
         }
+        
         // console.log(curPage);
     }
     const onChangeAnswerTarget=(event,answer)=>{
@@ -80,6 +130,26 @@ const EditSnackSnake = ({selectedWidget,curPage,add,setAdd,pages})=>{
         }
         // console.log(curPage);
     }
+    const addText=()=>{
+        for(let i=0;i<curPage.widgets.length;i++){
+            if(curPage.widgets[i]===selectedWidget){
+                let ob={"rightText":"","wrongText":""}
+                curPage.widgets[i].internals.options.push(ob);
+                setUpdate(update+1);
+                break;
+            }
+        }
+    }
+    const addImage=()=>{
+        for(let i=0;i<curPage.widgets.length;i++){
+            if(curPage.widgets[i]===selectedWidget){
+                let ob={"rightImage":"","wrongImage":""}
+                curPage.widgets[i].internals.options.push(ob);
+                setUpdate(update+1);
+                break;
+            }
+        }
+    }
     let text="";
     if(selectedWidget.internals.rightAnswer.actionType==='P'){
         for(let i=0;i<pages.length;i++){
@@ -91,16 +161,41 @@ const EditSnackSnake = ({selectedWidget,curPage,add,setAdd,pages})=>{
     }else{
         text=selectedWidget.internals.rightAnswer.target;
     }
-
+    // console.log(textArray);
     let textArray=[]
-    for(let i=0;i<2;i++){
-        let ob={"rightImage":"","wrongImage":""}
-        if(i<selectedWidget.internals.options.length){
-            ob.rightImage=selectedWidget.internals.options[i].rightImage;
-            ob.wrongImage=selectedWidget.internals.options[i].wrongImage;
+    console.log(selectedWidget.internals.options);
+    for(let i=0;i<selectedWidget.internals.options.length;i++){
+        if(selectedWidget.internals.options[i].rightImage===undefined){
+            textArray.push(selectedWidget.internals.options[i]);
+        }else{
+            console.log(i);
+            let ob={"rightImage":"","wrongImage":""};
+            console.log(selectedWidget.internals.options);
+            if(selectedWidget.internals.options[i].rightImage!==""){
+                axios_instance({
+                    method: 'get',
+                    url: "media/"+encodeURIComponent(selectedWidget.internals.options[i].rightImage),
+                }).then((res)=>{
+                    ob.rightImage=res.data.data;
+                }).catch((err)=>{
+                    console.log(err);
+                });
+            }
+            if(selectedWidget.internals.options[i].wrongImage!==""){
+                axios_instance({
+                    method: 'get',
+                    url: "media/"+encodeURIComponent(selectedWidget.internals.options[i].wrongImage),
+                }).then((res)=>{
+                    ob.wrongImage=res.data.data;
+                }).catch((err)=>{
+                    console.log(err);
+                });
+            }
+            console.log(ob);
+            textArray.push(ob);
         }
-        textArray.push(ob);
     }
+    console.log(textArray);
     let game=<div>
                 <Table striped bordered hover>
                     <thead>
@@ -125,19 +220,50 @@ const EditSnackSnake = ({selectedWidget,curPage,add,setAdd,pages})=>{
                         <th>Wrong Opstion</th>
                         </tr>
                     </thead>
-                    <tbody>
+                    {/* <tbody>
                         <tr>
-                        <td><input defaultValue={textArray[0].rightImage} onChange={(event)=>{onChangeSnack(event,"correct",0)}} className='inputStyle' type="text" id="correctText1" name="correctText"/></td>
-                        <td><input defaultValue={textArray[0].wrongImage} onChange={(event)=>{onChangeSnack(event,"wrong",0)}} className='inputStyle' type="text" id="wrongText1" name="wrongText"/></td>
+                        <td><input onChange={(event)=>{onChangeSnack(event,"correct",0)}} className='inputStyle' type="text" id="correctText1" name="correctText"/></td>
+                        <td><input onChange={(event)=>{onChangeSnack(event,"wrong",0)}} className='inputStyle' type="text" id="wrongText1" name="wrongText"/></td>
                         </tr>
-                    </tbody>
+                    </tbody> */}
                     <tbody>
-                        <tr>
-                        <td><input defaultValue={textArray[1].rightImage} onChange={(event)=>{onChangeSnack(event,"correct",1)}} className='inputStyle' type="text" id="correctText2" name="correctText"/></td>
-                        <td><input defaultValue={textArray[1].wrongImage} onChange={(event)=>{onChangeSnack(event,"wrong",1)}} className='inputStyle' type="text" id="wrongText2" name="wrongText"/></td>
-                        </tr>
+                        {console.log(textArray)}
+                        {
+                            textArray.map((x,i) => {
+                                if(x.rightImage===undefined){
+                                    return (
+                                        <tr key={i}>
+                                            <td><input defaultValue={x.rightText} onChange={(event)=>{onChangeSnack(event,"correct",i,"text")}} className='inputStyle' type="text" name="correctText"/></td>
+                                            <td><input defaultValue={x.wrongText} onChange={(event)=>{onChangeSnack(event,"wrong",i,"text")}} className='inputStyle' type="text" name="wrongText"/></td>
+                                        </tr>
+                                    );
+                                }else{
+                                    console.log(x);
+                                    return (
+                                        <tr key={i}>
+                                            {/* <td>
+                                                <button className='deleteButton' onClick={(event)=>{handleClick(event,i)}}><img src={x.rightImage} height='50px' width='50%' alt="image"/></button>
+							                    <input type="file" ref={hiddenFileInput} style={{ display: "none" }} onChange={(event)=>{onChangeSnack(event,"correct",i,"image")}} />
+                                            </td>
+                                            <td>
+                                                <button className='deleteButton' onClick={handleClick}><img src={x.wrongImage} height='50px' width='50%' alt="image"/></button>
+							                    <input type="file" ref={hiddenFileInput} style={{ display: "none" }} onChange={(event)=>{onChangeSnack(event,"wrong",i,"image")}} />
+                                            </td> */}
+                                            <td><input onChange={(event)=>{onChangeSnack(event,"correct",i,"image")}} className='inputStyle' type="file" name="correctText"/><img src={textArray[i].rightImage} height='50px' width='50%' alt=''/></td>
+                                            <td><input onChange={(event)=>{onChangeSnack(event,"wrong",i,"image")}} className='inputStyle' type="file" name="wrongText"/><img src={textArray[i].wrongImage} height='50px' width='50%' alt=''/></td>
+                                        </tr>
+                                    );
+                                }
+                                
+                            })
+                        }
+                        
                     </tbody>
                 </Table>
+                <div style={{justifyContent:'space-around',display:'flex'}}>
+                    <Button onClick={()=>{addText()}}>Add Text</Button>
+                    <Button onClick={()=>{addImage()}}>Add Image</Button>
+                </div>
             </div>
     return(game);
 }
